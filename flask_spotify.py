83d794a69f36172c21e97c9007a71f6a24b9a7b5
jaimieonigkeit <jaimie.onigkeit@gmail.com>
2020-07-02 21:31:30 -0600

    Refactor

--- flask_spotify.py
+++ flask_spotify.py
@@ -1,30 +1,22 @@
 from flask import Flask, jsonify, request, render_template
 import pickle
-import viz
 import json
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from model import neighbor, result_id_dict, clean_data_split, encoded_full, getPred, df, mood_mult, mood_check
-#from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def main():
-#  return render_template("main.html")
     req = request.json.get("favorite_songs")
     json_format = json.dumps(req)
     df = pd.read_json(json_format)
     mood_req = request.json.get("mood")
-    print(req)
-    print(mood_req)
-
-    if mood_req != None:
+    if mood_check(mood_req[0]) == True:
         mood_req_format = json.dumps(mood_req)
         mood_df = pd.read_json(mood_req_format)
-        mood_df = mood_df.fillna(0)
         df = mood_mult(df, mood_df)
-#    data = pd.read_json(req, orient='split')
     print(df)
     suggested_songs = getPred(df, neighbor, result_id_dict,
                               clean_data_split, encoded_full)
