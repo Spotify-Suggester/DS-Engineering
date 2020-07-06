e18f7928aa2f93c216c0b76a6755f04ec91eaeda
jaimieonigkeit <jaimie.onigkeit@gmail.com>
2020-07-06 09:44:52 -0600

    New model

--- flask_spotify.py
+++ flask_spotify.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-import pickle
 import json
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
-from model import neighbor, result_id_dict, clean_data_split, encoded_full, getPred, df, mood_mult, mood_check
+from model import neighbor, id_dict, clean_scaler, yr_model, yr_scaler, result_id_dict, clean_function, encoder_updated, getPred, mood_mult, mood_check
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def main():
+    #Takes POST from webpage, checks for mood data, multiplies mood rows
     req = request.json.get("favorite_songs")
     json_format = json.dumps(req)
     df = pd.read_json(json_format)
@@ -18,8 +18,12 @@ def main():
         mood_df = pd.read_json(mood_req_format)
         df = mood_mult(df, mood_df)
     print(df)
-    suggested_songs = getPred(df, neighbor, result_id_dict,
-                              clean_data_split, encoded_full)
+
+    #Returns suggested songs
+    suggested_songs = getPred(
+        df, neighbor_model=neighbor, result_id_dict=id_dict, 
+        cleanFunction=clean_function, clean_scaler=clean_scaler, 
+        encoder=encoder_updated, model_yr=yr_model, yr_scaler=yr_scaler)
     print(suggested_songs)
     return jsonify(suggested_songs)
 
--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 import pickle
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+import numpy as np
 from tensorflow.keras.models import load_model
 
 df = pd.read_csv(
@@ -10,84 +11,24 @@ df = pd.read_csv(
 
 first_five = df.iloc[:5]
 
-# getting the clean function
-def clean_data_split(df, use_test_split=False):
-  """
-  This is the function that will clean the data getting it
-  ready for use
-​
-  use_test_split: is set to False and will not split the data.
-  When changed to True, will split the data with 5 percent as a test data
-​
-  If use_test_split is False then will return X, y
-  If use_test_split is True will return x_train, x_test, y_train, y_test
-​
-  """
-  dff = df.copy()
-  # removed artists, and id from the features
-  features = [
-      "acousticness", "popularity", "danceability", "duration_ms", "energy",
-      "instrumentalness", "key", "liveness", "loudness", "mode",
-      "speechiness", "tempo", "valence"
-  ]
-  if use_test_split == True:
+#Variable imports from pickle files
+infile = open("clean_scaler.pkl",'rb')
+clean_scaler = pickle.load(infile)
+infile.close()
 
-    target = ["id", "name"]
-  else:
-    target = ["id"]
+infile = open("result_yr_dict.pkl",'rb')
+id_dict = pickle.load(infile)
+infile.close()
 
-  X = dff[features]
-  y = dff[target]
-  # won't encode the artists
+infile = open("yr_scaler.pkl", "rb")
+yr_scaler = pickle.load(infile)
+infile.close()
 
-  scaler = MinMaxScaler()
-  X = scaler.fit_transform(X)
-  # doing the normalization of the data
-  #normalizer = Normalizer("max")
-  #X = normalizer.fit_transform(X)
-  if use_test_split == True:
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.05,
-                                                        random_state=42)
-    return x_train, x_test, y_train, y_test
+encoder_updated = load_model('encoder_updated')
 
-  return X,  y
+yr_model = load_model("year_model")
 
-
-def getPred(playList, neigbor, result_id_dict,  cleanfunc, encoder_model, num_songs_to_return=5):
-  """
-    This function will return a list of the songs that are suggested
-  """
-  # Will run each of the songs through the
-  # clean method and then will send it through the
-  # encoder
-  x, y = cleanfunc(playList, use_test_split=False)
-
-  playList_results = encoder_model.predict(x)
-
-  summed_val = 0
-  # will loop through the playlist and get the average
-  for result in playList_results:
-    summed_val = summed_val + result
-  # getting the average of the each of the values
-  avg_val = summed_val/len(playList)
-  # running the avg_val into the nearest neighbor
-  closest_ones = neigbor.kneighbors([avg_val], return_distance=False)
-  # making the list of ids to check
-  y_list = y["id"].tolist()
-
-  suggested_songs = []
-  # Looping through the array of the closest_ones
-  for i in range(len(closest_ones[0])):
-
-    x = result_id_dict.get(closest_ones[0][i])
-    if x not in y_list:
-      suggested_songs.append(x)
-    if len(suggested_songs) == num_songs_to_return or i == len(closest_ones[0])-1:
-      break
-  return suggested_songs
-
-
-infile = open("neighbor_model.pkl", "rb")
+infile = open("neighbor_updated.pkl", "rb")
 neighbor = pickle.load(infile)
 infile.close()
 
@@ -95,13 +36,92 @@ infile = open("getIdDict.pkl", "rb")
 result_id_dict = pickle.load(infile)
 infile.close()
 
-def mood_mult(df, mood):
-    new_mood = pd.DataFrame()
-    for i in range(len(df)):
-        new_mood = pd.concat([new_mood, mood])
-    df = pd.concat([df, new_mood])
+# getting the clean function
+def clean_function(df, scaler=None, use_test_split=False):
+  """
+    This function will clean and prepare the data for the prediction function.
+    If use_test_split is False then will return X, y.
+    If use_test_split is True will return x_train, x_test, y_train, y_test.
+    """
+  dff = df.copy()
 
-    return df
+  # removing any pause track
+  dff = dff[(df["name"] != "Pause Track") & (dff["name"] != "Pause Track - Live")]
+
+  # target for the first model will be the int of the year released.
+  # the target is only for the training aspect
+  target = ["name", "id"]
+
+  features = [
+    "acousticness", "popularity", "danceability", "duration_ms", "energy",
+     "instrumentalness", "key", "liveness", "loudness", "mode","speechiness",
+     "tempo", "valence"
+              ]
+
+  X = dff[features]
+  y = dff[target]
+  # doing the scaling of the data
+  #scaler = MinMaxScaler()
+  #scaler = StandardScaler()
+  X = scaler.fit(X).transform(X)
+
+  if use_test_split == True:
+    x_train , x_test, y_train, y_test = train_test_split(
+      X, y, test_size=.05, random_state=42
+      )
+    return x_train, x_test, y_train, y_test
+
+  return X, y
+
+
+def getPred(playlist, neighbor_model, result_id_dict, cleanFunction, clean_scaler, encoder,
+            model_yr=None, yr_scaler=None, numsongs_return=10):
+  """
+  This is the function that will be used
+  for predicting with using the  year and or explicit features
+  """
+  # coming out of this function the x is scaled
+  # with the standard scaler
+  x, y = cleanFunction(playlist, scaler=clean_scaler)
+  yr_result = None
+  year_empty = 1
+
+  # choosing the type of encoder to use
+  if model_yr != None:
+    yr_result = model_yr.predict(x)
+    yr_result = yr_scaler.fit(yr_result).transform(yr_result)
+    year_empty = 0
+
+  # Now need to check if the there are values in the yr_result and the 
+  # explicit_result
+  if year_empty == 0:
+    x = np.hstack((x,yr_result))
+
+  # Will now get the prediction with the encoder
+  playList_results = encoder.predict(x)
+
+  summed_val = 0
+  # will loop through the playlist and get the average
+  for result in playList_results:
+    summed_val = summed_val + result
+  # getting the average of the each of the values
+  avg_val = summed_val/len(playlist)
+  # running the avg_val into the nearest neighbor
+  closest_ones = neighbor_model.kneighbors(
+    [avg_val], return_distance=False, n_neighbors=10
+    )
+  # making the list of ids to check
+  y_list = y["id"].tolist()
+
+  suggested_songs = []
+  # Looping through the array of the closest_ones
+  for i in range(len(closest_ones[0])):
+    x = result_id_dict.get(closest_ones[0][i])
+    if x not in y_list:
+      suggested_songs.append(x)
+    if len(suggested_songs) == numsongs_return or i == len(closest_ones[0])-1:
+      break
+  return suggested_songs
 
 def mood_check(mood_dict):
     for key in mood_dict:
@@ -109,17 +129,26 @@ def mood_check(mood_dict):
             return True
     return False
 
+def mood_mult(df, mood):
+    new_mood = pd.DataFrame()
+    for i in range(len(df)):
+        new_mood = pd.concat([new_mood, mood])
+    df = pd.concat([df, new_mood])
+    return df
 
 encoded_full = load_model('encoded_complete', compile=False)
 
 suggested_songs = getPred(
-    first_five, neighbor, result_id_dict, clean_data_split, encoded_full)
-
+    df, neighbor_model=neighbor, result_id_dict=id_dict, 
+    cleanFunction=clean_function, clean_scaler=clean_scaler, 
+    encoder=encoder_updated, model_yr=yr_model, yr_scaler=yr_scaler)
 
 
 
 if __name__ == "__main__":
     suggested_songs = getPred(
-        first_five, neighbor, result_id_dict, clean_data_split, encoded_full)
+        df, neighbor_model=neighbor, result_id_dict=id_dict, 
+        cleanFunction=clean_function, clean_scaler=clean_scaler, 
+        encoder=encoder_updated, model_yr=yr_model, yr_scaler=yr_scaler)
 
     print(suggested_songs)
